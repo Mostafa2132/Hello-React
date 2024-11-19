2d7052289be9e9fd53bea4e46ce501b0542219dd
Mostafa2132 <12m0stafa7@gmail.com>
2024-11-20 01:49:11 +0200

    frist

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,17 +1,15 @@
 
 
-
-
-
-import { useState, useEffect } from "react";
-import { NavLink, Link } from "react-router-dom";
+import  { useState, useEffect } from "react";
+import { Link, NavLink } from "react-router-dom";
 
 function Navbar() {
   const [scrolled, setScrolled] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
-      setScrolled(window.scrollY > 10); 
+      setScrolled(window.scrollY > 10);
     };
 
     window.addEventListener("scroll", handleScroll);
@@ -39,52 +37,53 @@ function Navbar() {
           <div className="sm:hidden">
             <button
               type="button"
-              className="hs-collapse-toggle relative size-10 flex justify-center items-center gap-x-2 rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-transparent dark:border-neutral-700 dark:text-white dark:hover:bg-white/10 dark:focus:bg-white/10"
-              id="hs-navbar-example-collapse"
-              aria-expanded="false"
-              aria-controls="hs-navbar-example"
+              onClick={() => setIsMenuOpen(!isMenuOpen)}
+              className="relative size-10 flex justify-center items-center gap-x-2 rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-transparent dark:border-neutral-700 dark:text-white dark:hover:bg-white/10 dark:focus:bg-white/10"
+              aria-expanded={isMenuOpen}
               aria-label="Toggle navigation"
-              data-hs-collapse="#hs-navbar-example"
             >
-              <svg
-                className="hs-collapse-open:hidden shrink-0 size-4"
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <line x1="3" x2="21" y1="6" y2="6" />
-                <line x1="3" x2="21" y1="12" y2="12" />
-                <line x1="3" x2="21" y1="18" y2="18" />
-              </svg>
-              <svg
-                className="hs-collapse-open:block hidden shrink-0 size-4"
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <path d="M18 6 6 18" />
-                <path d="m6 6 12 12" />
-              </svg>
+              {!isMenuOpen ? (
+                <svg
+                  className="shrink-0 size-4"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <line x1="3" x2="21" y1="6" y2="6" />
+                  <line x1="3" x2="21" y1="12" y2="12" />
+                  <line x1="3" x2="21" y1="18" y2="18" />
+                </svg>
+              ) : (
+                <svg
+                  className="shrink-0 size-4"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <path d="M18 6 6 18" />
+                  <path d="m6 6 12 12" />
+                </svg>
+              )}
               <span className="sr-only">Toggle navigation</span>
             </button>
           </div>
         </div>
         <div
-          id="hs-navbar-example"
-          className="hidden hs-collapse overflow-hidden transition-all duration-300 basis-full grow sm:block"
-          aria-labelledby="hs-navbar-example-collapse"
+          className={`${
+            isMenuOpen ? "block" : "hidden"
+          } overflow-hidden transition-all duration-300 basis-full grow sm:block`}
         >
           <div className="flex flex-col gap-5 mt-5 sm:flex-row sm:items-center sm:justify-end sm:mt-0 sm:ps-5">
             <NavLink
@@ -100,7 +99,6 @@ function Navbar() {
             >
               About
             </NavLink>
-
             <NavLink
               className={({ isActive }) =>
                 `font-medium text-xl uppercase text-white focus:outline-none ${
@@ -133,3 +131,4 @@ function Navbar() {
 }
 
 export default Navbar;
+
